44b5bc76c5ea5a52faea1f67e71feea1ddf424cd
MatthewSale <74600474+MatthewSale@users.noreply.github.com>
2022-04-15 23:30:21 +0100

    experemtnts with the various camras

--- Platformer/Assets/PortalCamera.cs
+++ Platformer/Assets/PortalCamera.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 public class PortalCamera : MonoBehaviour {
 
 	public Transform playerCamera;
+	public GameObject Player_modle;
 	public Transform portal;
 	public Transform otherPortal;
+	Transform offset_holder;
+	int player_height;
 	
-	// Update is called once per frame
 	void Update () {
-		Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
-		transform.position = portal.position + playerOffsetFromPortal;
+		Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;		 
+		this.transform.position = portal.position + playerOffsetFromPortal;
+		
+
+
 
 		float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
 
 		Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
-		Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
-		transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+		Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;		
+		this.transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
 	}
 }
